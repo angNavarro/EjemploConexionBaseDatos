a52d66fba25a14a83fea8fabcdf0e0091b88dbe4
angNavarro <angelaldarias.alu@iespacomolla.es>
2024-02-25 10:12:03 +0100

    Actualizacion de try catch

--- ProyectoConexion.java
+++ ProyectoConexion.java
@@ -22,8 +22,10 @@ private final String url="jbdc:sqlite:database.db"
                 "carrera text NOT NULL,\n" +
                 ");";
 
-        try (Connection conn = this.connect();
+        try (
+                Connection conn = this.connect();
                 Statement stmt = conn.createStatement()) {
+
             stmt.execute(sql);
 
         } catch (SQLException e) {
@@ -35,14 +37,97 @@ private final String url="jbdc:sqlite:database.db"
     public void insert(String nombre, String carrera) {
         String sql = "INSERT INTO alumnos(nombre, carrera) VALUES(?,?)";
 
-        try (Connection conn = this.connect();
+        try (
+                Connection conn = this.connect();
                 PreparedStatement pstmt = conn.PreparedStatement(sql)) {
+
             pstmt.setString(1, nombre);
             pstmt.setString(2, carrera);
             pstmt.executeUpdate();
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
-}}
+
+    // Select all rows in alumnos table
+    public void selectAll() {
+        String sql = "SELECT id, nombre, carrera FROM alumnos";
+
+        try (
+                Connection conn = this.connect();
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + "\t" +
+                        rs.getString("nombre") + "\t" +
+                        rs.getString("carrera"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // Update data of alumno specified by id
+    public void update(int id, String nombre, String carrera) {
+        String sql = "UPDATE alumnos SET nombre = ?, carrera = ? WHERE id = ?";
+
+        try (
+            Connection conn = this.connect();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, nombre);
+            pstmt.setString(2, carrera);
+            pstmt.setString(3, id);
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    // Delete alumno specified by id
+    public void delete(int id) {
+        String sql = "DELETE FROM alumnos WHERE ID=?";
+
+        try (
+            Connection conn = this.connect();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        ProyectoConexion app = new ProyectoConexion();
+
+        // Create new table
+        app.createNewTable();
+
+        // Insert new data
+        app.insert("Angel Aldarias", "Ingenieria software");
+        app.insert("Eva Nuez", "Teleco");
+
+        // Select all data
+        System.out.println("Lista alumnos: ");
+        app.selectAll();
+
+        // Update data
+        app.update(1, "Juan perez", "Teleco");
+        System.out.println("Datos actualizados: ");
+        app.selectAll();
+
+        // Delete student
+        app.delete(2);
+        System.out.println("Listado de alumnos actualizado: ");
+        app.selectAll();
+    }
+
+}
